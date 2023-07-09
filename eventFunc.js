9afee6b4eecd516dc93dcda25d36a1aba3aeca62
unknown <mitrovicbojan87@gmail.com>
2023-07-09 15:26:28 +0300

    mouse

--- eventFunc.js
+++ eventFunc.js
@@ -1,19 +1,19 @@
-// const clickMe = (button) => {
-//   console.log(button.innerText);
-// };
+const clickMe = (button) => {
+  console.log(button.innerText);
+};
 
-// let btns = document.querySelectorAll("button");
+let btns = document.querySelectorAll("button");
 
-// btns.forEach(function (btn) {
-//   btn.addEventListener("click", function (event) {
-//     // console.log(btn.innerText);
-//     let clickedBtn = event.target;
-//     console.log(clickedBtn.innerText);
-//   });
-// });
-// btn.addEventListener("click", function () {
-//   console.log("click");
-// });
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (event) {
+    // console.log(btn.innerText);
+    let clickedBtn = event.target;
+    console.log(clickedBtn.innerText);
+  });
+});
+btn.addEventListener("click", function () {
+  console.log("click");
+});
 let link = document.querySelector("#link");
 
 link.addEventListener("click", (event) => {
@@ -31,4 +31,23 @@ let options = document.querySelector("select");
 
 options.addEventListener("change", (event) => {
   console.log(event.target.value);
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 1000) {
+    console.log("manje od 1000");
+  }
+});
+
+let input = document.querySelector("input");
+
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Delete") {
+    alert("you have deleted something.");
+  }
+  console.log(event.key);
+});
+
+input.addEventListener("mousemove", (event) => {
+  console.log("mouse move");
 });
