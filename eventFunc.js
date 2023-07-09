79d34c4e945b47d9cb52f2fcd64d4accc6a19e8c
unknown <mitrovicbojan87@gmail.com>
2023-07-09 14:39:42 +0300

    event

--- eventFunc.js
+++ eventFunc.js
@@ -1,3 +1,14 @@
-const clickMe = () => {
-  console.log("click");
-};
+// const clickMe = (button) => {
+//   console.log(button.innerText);
+// };
+
+let btns = document.querySelectorAll("button");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    console.log(btn.innerText);
+  });
+});
+// btn.addEventListener("click", function () {
+//   console.log("click");
+// });
